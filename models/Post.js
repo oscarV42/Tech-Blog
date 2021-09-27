660132dae14fedb2b22e40ee967015e5ecefb817
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-27 02:16:31 -0400

    updating Post model and homeRoutes,js file name

--- models/Post.js
+++ models/Post.js
@@ -7,7 +7,7 @@ const sequelize = require('../config/connection');
 class Post extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
+Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
--- models/index.js
+++ models/index.js
@@ -11,22 +11,22 @@ User.hasMany(Post, {
   foreignKey: 'User_id',
   onDelete: 'CASCADE',
 })
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag, 
-    unique: false,
-  },
-  as: 'product_tags'
-})
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag, 
-    unique: false,
-  },
-  as: 'tag_products'
-})
+// // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Tag, {
+//   through: {
+//     model: ProductTag, 
+//     unique: false,
+//   },
+//   as: 'product_tags'
+// })
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(Product, {
+//   through: {
+//     model: ProductTag, 
+//     unique: false,
+//   },
+//   as: 'tag_products'
+// })
 
 module.exports = {
   User, 
