88eca886dddbad210bbaad5c570eb8c9d8af57f3
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-27 00:47:16 -0400

    defining columns in Post model

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Post extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_body: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },   
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+        unique: 'false'
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
