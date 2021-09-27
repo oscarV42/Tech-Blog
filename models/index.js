3f18b448202a988b7f4dca1ee63e5ecbd41e7cc9
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-09-27 00:52:27 -0400

    creating fetch associations between models

---
+++ models/index.js
@@ -0,0 +1,34 @@
+// import models
+const User = require('./User');
+const Post = require('./Post');
+
+// Products belongsTo User
+Post.belongsTo(User, {
+  foreignKey: 'User_id'
+})
+// Categories have many Products
+User.hasMany(Post, {
+  foreignKey: 'User_id',
+  onDelete: 'CASCADE',
+})
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag, 
+    unique: false,
+  },
+  as: 'product_tags'
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag, 
+    unique: false,
+  },
+  as: 'tag_products'
+})
+
+module.exports = {
+  User, 
+  Post
+};
